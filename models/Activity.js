659339dd0313b6af0249325bbfb8807029d92650
Lucy <lucy_oswald@hotmail.co.uk>
2023-03-12 10:49:35 +0000

    Updated activity model

--- models/Activity.js
+++ models/Activity.js
@@ -1,6 +1,30 @@
 import mongoose from "mongoose";
 
-const activitySchema = mongoose.Schema({});
+const activitySchema = mongoose.Schema({
+  category: { type: String, required: true },
+  activityCountry: { type: String, required: true },
+  name: { type: String, required },
+  description: { type: String, required: true },
+  location: { type: String, required: true },
+  // price: {type: Number, required: true},
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+    get: (val) =>
+      new Intl.NumberFormat("en-UK", {
+        style: "currency",
+        currency: "GBP",
+      }).format(val),
+  },
+  imageUrl: { type: String, required: false },
+  linkToWebsite: { type: String, required: false },
+  createdBy: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
 
 const Activity = mongoose.model("Activity", activitySchema);
 
