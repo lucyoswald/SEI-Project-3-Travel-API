5271c881b9a479e9307e6ed8de4dfd140d9a9081
Lucy <lucy_oswald@hotmail.co.uk>
2023-03-12 12:46:15 +0000

    activity controller, error handler, router

--- router.js
+++ router.js
@@ -1,7 +1,16 @@
 import { Express } from "express";
+import activityController from "./controllers/activityController.js";
 
 const router = express.Router();
 
 router.route("/countriesInfo").get(countryController.getAllCountryData);
+
+// Activity routes
+//Not sure about this route think we need seeding data to move forward
+router
+  .route("/activities")
+  .post(activityController.addActivity)
+  .patch(activityController.updateActivity)
+  .delete(activityController.deleteActivity);
 
 export default router;
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { connectToDb } from "./utils/db.js";
 import logger from "./middleware/logger.js";
+import errorHandler from "./middleware/errorhandler.js";
 const app = express();
 const PORT = 6000;
 
 app.use(express.json());
 app.use(logger);
+app.use(errorHandler);
 
 const startServer = async () => {
   await connectToDb();
