5bb9630e3c24dc73ee6a8f811802a969944f6a83
Kirstin <kpetersen@elon.edu>
2023-03-13 18:24:10 +0000

    validationMiddleware

--- router.js
+++ router.js
@@ -1,18 +1,17 @@
 import userController from "./controllers/userController.js";
-
 import express from "express";
 import countryController from "./controllers/countryController.js";
 import activityController from "./controllers/activityController.js";
 import auth from "./middleware/auth.js";
-
-
+import validate from "./middleware/validate.js";
+import { body, validationResult } from "express-validator";
 
 const router = express.Router();
 
 router
   .route("/countries")
   .get(countryController.getAllCountryData)
-  .post(countryController.create);
+  .post(auth, countryController.create);
 router.route("/countries/:id").get(countryController.getById);
 
 // Activity routes
@@ -23,7 +22,14 @@ router
   .patch(activityController.updateActivity)
   .delete(activityController.deleteActivity);
 
-router.route("/register").post(userController.register);
+router
+  .route("/register")
+  .post(
+    body("email").isEmail(),
+    body("role").not().contains("admin"),
+    validate,
+    userController.register
+  );
 router.route("/login").post(userController.login);
 
 export default router;
