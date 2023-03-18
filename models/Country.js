1856107968d0e7ea6703ea672e153735244be4c4
Kirstin <kpetersen@elon.edu>
2023-03-18 16:05:55 +0000

    countrySchema updated

--- models/Country.js
+++ models/Country.js
@@ -5,6 +5,8 @@ const countrySchema = mongoose.Schema(
     name: { type: String, required: true, unique: true },
     currencyName: { type: String, required: true },
     continent: { type: String, required: true },
+    image: { type: String, required: true },
+    text: { type: String, required: true },
     createdAt: { type: Date, default: Date.now(), required: true },
     createdBy: { type: mongoose.Schema.ObjectId, ref: "User", required: true },
     //   comments: [commentsSchema] -> May or not may not use - delete or update once we've decided
