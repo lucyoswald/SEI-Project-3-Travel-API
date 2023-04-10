42bd15c94e2c1e2b24a62793c51870f3d5b4bf91
Lucy <lucy_oswald@hotmail.co.uk>
2023-04-10 18:25:57 +0100

    edits to activity card

--- controllers/userController.js
+++ controllers/userController.js
@@ -132,6 +132,7 @@ const login = async (req, res, next) => {
     return res.status(200).json({
       message: `${user.userName} is now logged in`,
       userId: `${user._id}`,
+      role: `${user.role}`,
       token,
     });
   } catch (err) {
